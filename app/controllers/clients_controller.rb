4b94e316997ca635df1755ce0a0abd4c63dcdd8c
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-01 21:55:19 -0700

    root spec green

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,7 @@
+class ClientsController < ApplicationController
+
+  def index
+#    @clients = Clients.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
    root 'clients#index'
 
-   
+   resources :clients
 
 end
