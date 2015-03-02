1852a2070e78d53901b3e373190d305998f90256
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-01 22:30:03 -0700

    migrated

--- db/migrate/20150302052002_create_clients.rb
+++ db/migrate/20150302052002_create_clients.rb
@@ -1,6 +1,17 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.string :client_id
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :member_id
+      t.datetime :date_of_birth
+      t.string :emergency_contact
+      t.string :emergency_number
+      t.text :additional_info
+
+      t.timestamps null: false
     end
   end
 end
