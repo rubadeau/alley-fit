2e1fbc2bc42e9b54a18921a3e743e6f0d15c7a43
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-02 17:52:59 -0700

    new client form partial

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,20 @@
 class ClientsController < ApplicationController
 
   def index
-#    @clients = Clients.all
+   @clients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to cleints_path, notice: 'Client created successfully!'
+    else
+      render :new
+    end
   end
 
 end
--- spec/features/clients_spec.rb
+++ spec/features/clients_spec.rb
@@ -8,18 +8,9 @@ feature 'Events' do
     expect(page).to have_content('Active Clients')
   end
 
-  scenario 'index lists all active clients with name' do
-    client_1 = Client.new(client_id: '1', name: 'Unhealthy Annie')
-    client.save!
-
-    expect(current_path).to eq clients_path
-
-    expect(page).to have_content 'Unhealthy Annie'
-    expect(page).to have_link 'New Cheese'
-  end
-
   scenario 'can make a new client from the new client form' do
-    click_link 'New Client'
+    visit root_path
+    click_button 'New Client'
 
     expect(current_path).to eq new_client_path
 
@@ -29,15 +20,6 @@ feature 'Events' do
     expect(current_path).to eq clients_path
     expect(page).to have_content 'Cheese created successfully!'
     expect(page).to have_content 'Overweight Dan'
-  end
 
-  scenario 'index links to show via the clients name' do
-    client_1 = Client.new(client_id: '1', name: 'Unhealthy Annie')
-    client.save!
-
-    click_link 'Unhealthy Annie'
-
-    expect(current_path).to eq client_path(client)
-    expect(page).to have_content 'Unhealthy Annie'
   end
 end
