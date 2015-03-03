a610869beafa9ce2e21895316d09ea2657cd3fc1
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-02 18:03:15 -0700

    Show complete

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,10 +11,41 @@ class ClientsController < ApplicationController
   def create
     @client = Client.new(client_params)
     if @client.save
-      redirect_to cleints_path, notice: 'Client created successfully!'
+      redirect_to client_path(@client), notice: 'Client created successfully!'
     else
       render :new
     end
   end
 
-end
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      flash[:notice] = "Client updated successfully!"
+      redirect_to client_path(@client)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   client = Client.find(params[:id])
+   client.destroy
+   flash[:notice] = "Deleted Client"
+   redirect_to clients_path
+  end
+
+
+    private
+
+    def client_params
+      params.require(:client).permit(:name)
+    end
+  end
