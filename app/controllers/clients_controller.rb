4f862f9c0f9a39065e5c48fe2e0b548b43814621
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-02 20:41:03 -0700

    workout partial

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -19,6 +19,7 @@ class ClientsController < ApplicationController
 
   def show
     @client = Client.find(params[:id])
+    @workout = @client.workouts
   end
 
   def edit
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,7 @@ class WorkoutsController < ApplicationController
 
   def index
     @workouts = Workout.all
+    @clients = Client.all
   end
 
   def new
@@ -19,6 +20,34 @@ class WorkoutsController < ApplicationController
 
   def show
     @workout = Workout.find(params[:id])
+    @client = @workout.client
   end
 
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    if @workout.update(workout_params)
+      flash[:notice] = "Workout updated successfully!"
+      redirect_to workout_path(@workout)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   workout = Workout.find(params[:id])
+   workout.destroy
+   flash[:notice] = "Deleted Workout"
+   redirect_to workouts_path
+  end
+
+
+    private
+
+    def client_params
+      params.require(:workout).permit(:date, :time, :workout, :coments)
+    end
 end
