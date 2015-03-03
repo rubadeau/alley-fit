b95aa094db0d4d92a327e82d9944d133a9226f87
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-02 21:39:02 -0700

    migrated assessments table

---
+++ db/migrate/20150303042136_create_assessments.rb
@@ -0,0 +1,23 @@
+class CreateAssessments < ActiveRecord::Migration
+  def change
+    create_table :assessments do |t|
+      t.string :overhead_squat
+      t.string :single_leg_squat
+      t.decimal :right_hip_strength
+      t.decimal :left_hip_strength
+      t.string :shoulder_mobility
+      t.string :right_glute_activation
+      t.string :left_glute_activation
+      t.decimal :plank
+      t.decimal :right_plank
+      t.decimal :left_plank
+      t.string :leg_lowering
+      t.string :push_up
+      t.string :blood_pressure
+      t.integer :weight
+      t.string :height
+
+      t.timestamps null: false
+    end
+  end
+end
