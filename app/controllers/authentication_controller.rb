73684fbe2048b9a1c3db1ea8b011972b8a4b540f
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-28 00:02:58 -0600

    signin, sign up, sign out

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,25 @@
+class AuthenticationController < ApplicationController
+
+  def new
+    session[:client_id] = current_user
+  end
+
+  def create
+    client = Client.find_by(email: params[:email])
+    if client && client.authenticate(params[:password])
+      session[:client_id] = client.id
+      flash[:notice] ="You have successfully signed in"
+      redirect_to client_path(client)
+    else
+      @sign_in_error = "Email / Password combination is invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:client_id] = nil
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
+end
